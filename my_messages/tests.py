a3c727c6e4e1c2267d85be471d4558dc61986936
arjunsinghy96 <14bcs010@smvdu.ac.in>
2018-02-01 07:50:29 +0530

    my_messages: Add test for home redirect to leagues

--- my_messages/tests.py
+++ my_messages/tests.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve
 from my_messages import views
 
 class HomePageTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user',
+                                        password='test_user')
 
     def test_root_maps_to_home_page_view(self):
         found = resolve('/')
@@ -11,6 +16,15 @@ class HomePageTest(TestCase):
     def test_home_page_redirects_to_login(self):
         response = self.client.get('/', follow=True)
         self.assertRedirects(response, '/accounts/login/?next=/')
+
+    def test_home_page_redirects_to_leagues_page(self):
+        """
+        Tests that the home page redirects to the leagues page when the 
+        user is logged in.
+        """
+        self.client.login(username='test_user', password='test_user')
+        response = self.client.get('/', follow=True)
+        self.assertRedirects(response, '/leagues/')
 
 
 class ChatPageTest(TestCase):
