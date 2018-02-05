b2397965f5fe30d9b516af86513ad75e700c5ab4
arjunsinghy96 <14bcs010@smvdu.ac.in>
2018-02-05 15:19:33 +0530

    Add fixed header template

--- my_messages/static/js/script.js
+++ my_messages/static/js/script.js
@@ -1,5 +1,6 @@
 $('document').ready(function(){
     $('.modal').modal();
+    $('.dropdown-button').dropdown();
     var chatbox = document.getElementById('chat-container');
     chatbox.scrollTop = chatbox.scrollHeight;
 });
