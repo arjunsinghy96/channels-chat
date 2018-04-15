fd2480c3647a4b933c33c90f6b17b8e7b6110f04
Arjun Singh Yadav <arjunsinghy96@gmail.com>
2018-04-15 23:12:57 +0530

    Register all storage models

--- storage/admin.py
+++ storage/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from storage.models import League, Membership
+from storage.models import League, Membership, Message, Invite
 
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ('league', 'user', 'permissions', 'joined_at')
+
+@admin.register(Message)
+class Message(admin.ModelAdmin):
+    list_display = ('league', 'sender', 'sent_at')
+
+@admin.register(Invite)
+class InviteAdmin(admin.ModelAdmin):
+    list_display = ('league', 'user', 'permissions')
