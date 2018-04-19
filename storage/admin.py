869a4b4290f8bc0dcea0b401a679c2a62887d8b0
Arjun Singh Yadav <arjunsinghy96@gmail.com>
2018-04-19 05:41:27 +0530

    storage: Add search_fields to model admin

--- storage/admin.py
+++ storage/admin.py
@@ -5,10 +5,12 @@ from storage.models import League, Membership, Message, Invite
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
+    search_fields = ['name',]
 
 @admin.register(Membership)
 class MembershipAdmin(admin.ModelAdmin):
     list_display = ('league', 'user', 'permissions', 'joined_at')
+    search_fields = ('league__name', 'user__username')
 
 @admin.register(Message)
 class Message(admin.ModelAdmin):
@@ -17,3 +19,4 @@ class Message(admin.ModelAdmin):
 @admin.register(Invite)
 class InviteAdmin(admin.ModelAdmin):
     list_display = ('league', 'user', 'permissions')
+    search_fields = ('league__name', 'user__username')
