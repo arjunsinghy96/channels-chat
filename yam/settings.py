c0a11b7700e12365f8c20911b5aa89f287cc3ffe
Arjun Singh Yadav <arjunsinghy96@gmail.com>
2018-04-22 17:23:47 +0530

    settins: Use sendgrid email backend

--- yam/settings.py
+++ yam/settings.py
@@ -146,7 +146,8 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'channels-chat')
 # Redirect url after login
 LOGIN_REDIRECT_URL = '/dashboard/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', '')
 
 LOGIN_URL = '/login/'
 
